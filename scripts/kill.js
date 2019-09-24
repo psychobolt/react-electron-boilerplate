631a8f579f0aead681c851f03e499ff6df0aa386
psychobolt <psychobolt@gmail.com>
2019-09-24 08:22:11 -0700

    Send interupt signal on killing dev processes

--- scripts/kill.js
+++ scripts/kill.js
@@ -3,16 +3,18 @@ import terminate from 'terminate';
 
 import config from '../package.json';
 
+const SIGNAL = 'SIGINT';
+
 async function kill() {
   await findProcess('name', process.platform === 'win32' ? 'electron.exe' : '.bin/electron').then(async processes1 => {
     if (!processes1.length) {
       await findProcess('name', 'concurrently').then(processes2 => {
         const process = processes2.find(({ cmd }) => cmd.indexOf(config.scripts['build:dev']) > -1);
         if (process) {
-          terminate(process.pid);
+          terminate(process.pid, SIGNAL);
         }
       });
-      await findProcess('name', 'scripts/start.js').then(processes3 => processes3.forEach(({ pid }) => terminate(pid)));
+      await findProcess('name', 'scripts/start.js').then(processes3 => processes3.forEach(({ pid }) => terminate(pid, SIGNAL)));
     }
   });
 }
