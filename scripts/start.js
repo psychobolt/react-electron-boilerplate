9a69c9cb30159d103c2102d84340ccd9f0ecb009
psychobolt <psychobolt@gmail.com>
2019-09-25 13:44:31 -0700

    Use async wait

--- scripts/start.js
+++ scripts/start.js
@@ -9,24 +9,23 @@ const DEV_PORT = 3000;
 const root = path.resolve(__dirname, '..');
 const file = path.resolve(config.output.path, config.output.filename);
 
-console.log('Waiting on development server on port 3000...'); // eslint-disable-line no-console
+async function start() {
+  console.log('Waiting on development server on port 3000...'); // eslint-disable-line no-console
+  await waitOn({ resources: [`tcp:localhost:${DEV_PORT}`] });
+  const args = process.argv.length > 2 ? process.argv.slice(2).join(' ') : '';
+  const exec = `${electron} ${root} ${args}`;
+  console.log( // eslint-disable-line no-console
+    `Server found. Starting Electron app with watch mode. Watching file ${file}\
+    \n> ${exec}\
+    `,
+  );
+  nodemon({
+    exec,
+    watch: file,
+    stdout: true,
+  }).on('start', () => {
+    console.log('Application started.'); // eslint-disable-line no-console
+  });
+}
 
-waitOn(
-  { resources: [`tcp:localhost:${DEV_PORT}`] },
-  () => {
-    const args = process.argv.length > 2 ? process.argv.slice(2).join(' ') : '';
-    const exec = `${electron} ${root} ${args}`;
-    console.log( // eslint-disable-line no-console
-      `Server found. Starting Electron app with watch mode. Watching file ${file}\
-      \n> ${exec}\
-      `,
-    );
-    nodemon({
-      exec,
-      watch: file,
-      stdout: true,
-    }).on('start', () => {
-      console.log('Application started.'); // eslint-disable-line no-console
-    });
-  },
-);
+start();
