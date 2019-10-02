1db3dbaf3f8b231e1ee8fa1c85ce7f87ecac9b56
psychobolt <psychobolt@gmail.com>
2019-10-01 20:43:15 -0700

    export babel config as a function

--- babel.config.js
+++ babel.config.js
@@ -1,49 +1,49 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: 'current',
-      },
-      forceAllTransforms: isProduction,
-      useBuiltIns: 'entry',
-      corejs: '3.0.0',
-    }],
-    '@babel/preset-flow',
-  ],
-  plugins: [
-    // Stage 1
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-do-expressions',
-    // Stage 2
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true,
-      },
+module.exports = function config(api) {
+  return {
+    presets: [
+      ['@babel/preset-env', {
+        targets: {
+          node: 'current',
+        },
+        forceAllTransforms: api.env('production'),
+        useBuiltIns: 'entry',
+        corejs: '3.0.0',
+      }],
+      '@babel/preset-flow',
     ],
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-    // Stage 3
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-dynamic-import',
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
-    '@babel/plugin-proposal-json-strings',
-  ],
-  env: {
-    test: {
-      plugins: [
-        'dynamic-import-node',
+    plugins: [
+      // Stage 1
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-do-expressions',
+      // Stage 2
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true,
+        },
       ],
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      // Stage 3
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-syntax-dynamic-import',
+      [
+        '@babel/plugin-proposal-class-properties',
+        {
+          loose: true,
+        },
+      ],
+      '@babel/plugin-proposal-json-strings',
+    ],
+    env: {
+      test: {
+        plugins: [
+          'dynamic-import-node',
+        ],
+      },
     },
-  },
+  };
 };
