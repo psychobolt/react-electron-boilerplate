6bfb3ddc38e5e6125b813eb76fae186ae4f6984b
psychobolt <psychobolt@gmail.com>
2019-12-04 21:55:01 -0800

    Clean start and stop scripts

--- scripts/kill.js
+++ scripts/kill.js
@@ -1,16 +1,26 @@
-import electron from 'electron';
+import fs from 'fs';
+import path from 'path';
 import findProcess from 'find-process';
 import terminate from 'terminate';
 
 import config from '../package.json';
 
 const SIGNAL = 'SIGINT';
+const root = path.resolve(__dirname, '..');
+const pidFile = path.resolve(root, 'electron.pid');
 const debug = process.argv.indexOf('--debugger') > -1;
 
 async function kill() {
-  await findProcess('name', electron.replace(/\\/g, '\\\\')).then(async processes1 => {
+  const electronPID = fs.readFileSync(pidFile, { encoding: 'utf8' });
+  await findProcess('pid', electronPID).then(async processes1 => {
     let terminated = false;
-    if (debug ? !processes1.length : !debug) {
+    if (!debug) {
+      processes1.forEach(({ pid }) => {
+        terminate(pid, SIGNAL);
+        terminated = true;
+      });
+    }
+    if ((debug && !processes1.length) || !debug) {
       await findProcess('name', 'concurrently').then(processes2 => {
         const process = processes2.find(({ cmd }) => cmd.indexOf(config.scripts['build:dev']) > -1);
         if (process) {
--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,15 @@
+import fs from 'fs';
 import path from 'path';
 import electron from 'electron';
 import nodemon from 'nodemon';
 import waitOn from 'wait-on';
+import terminate from 'terminate';
 
 import config from '../webpack.main.babel';
 
 const DEV_PORT = 3000;
 const root = path.resolve(__dirname, '..');
+const pidFile = path.resolve(root, 'electron.pid');
 const file = path.resolve(config.output.path, config.output.filename);
 
 async function start() {
@@ -14,17 +17,27 @@ async function start() {
   await waitOn({ resources: [`http://localhost:${DEV_PORT}`] });
   const args = process.argv.length > 2 ? process.argv.slice(2).join(' ') : '';
   const exec = `${electron} ${root} ${args}`;
-  console.log( // eslint-disable-line no-console
-    `Server found. Starting Electron app with watch mode. Watching file ${file}\
-    \n> ${exec}\
-    `,
-  );
+  console.log('Server found.'); // eslint-disable-line no-console
   nodemon({
     exec,
     watch: file,
-    stdout: true,
+    signal: 'SIGINT',
+    verbose: true,
   }).on('start', () => {
     console.log('Application started.'); // eslint-disable-line no-console
+  }).on('restart', () => {
+    const pid = fs.readFileSync(pidFile, { encoding: 'utf8' });
+    terminate(pid, 'SIGINT', {}, () => {
+      console.log(`Process ${pid} killed.`); // eslint-disable-line no-console
+    });
+  }).on('log', event => {
+    console.log(`[nodemon] ${event.type} - ${event.message}`); // eslint-disable-line no-console
+    const key = 'pid:';
+    const startIndex = event.message.indexOf(key);
+    const pid = startIndex > -1 ? event.message.substring(startIndex + key.length).trim() : null;
+    if (pid) {
+      fs.writeFileSync(pidFile, pid);
+    }
   });
 }
 
